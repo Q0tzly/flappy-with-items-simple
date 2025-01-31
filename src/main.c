92782e49fb21d6d7acb2945673fcd0834365a89b
Q0tzly <g9164902@gmail.com>
2025-01-31 20:02:22 +0900

    update: Added Help function on title scene

--- src/main.c
+++ src/main.c
@@ -218,6 +218,22 @@ void draw_game_over_screen() {
     }
 }
 
+void draw_help_screen() {
+    clear();
+    mvprintw(LINES / 2 - 2, COLS / 2 - 10, "--- How to Play ---");
+    mvprintw(LINES / 2, COLS / 2 - 6, "Space: Jump");
+    mvprintw(LINES / 2 + 2, COLS / 2 - 17, "Press 'h' or 'q', Enter to return");
+    refresh();
+    int ch;
+    while (1) {
+        ch = getch();
+        if (ch == '\n' || ch == 'h' || ch == 'q') {
+            return;
+        }
+        usleep(DELAY);
+    }
+}
+
 void draw_ascii_title() {
     mvprintw(LINES / 2 - 9, (COLS - 40) / 2, "  ______ _                              ");
     mvprintw(LINES / 2 - 8, (COLS - 40) / 2, " /  ____| |         ___    ___   __  __ ");
@@ -231,8 +247,9 @@ void draw_ascii_title() {
 void draw_title_screen() {
     clear();
     draw_ascii_title();
-    mvprintw(LINES / 2 + 2, COLS / 2 - 11, "Press Enter to Start");
-    mvprintw(LINES / 2 + 3, COLS / 2 - 11, "Press 'q'   to Quit");
+    mvprintw(LINES / 2 + 2, COLS / 2 - 12, "Press Enter to  Start");
+    mvprintw(LINES / 2 + 3, COLS / 2 - 12, "Press 'h'   for Help");
+    mvprintw(LINES / 2 + 4, COLS / 2 - 12, "Press 'q'   to  Quit");
     refresh();
 
     int ch;
@@ -244,6 +261,11 @@ void draw_title_screen() {
         }
         if (ch == 'q') {
             game_over = 1;
+            return;
+        }
+        if (ch == 'h') {
+            draw_help_screen();
+            draw_title_screen();
             return;
         }
         usleep(DELAY);
