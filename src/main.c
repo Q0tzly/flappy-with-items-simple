8e601d743fc2559671ea5731cf363d4053b1b546
Q0tzly <g9164902@gmail.com>
2025-01-31 15:37:49 +0900

    update: Updated the Pipes, Replaced to #'s block from |'s stick

--- src/main.c
+++ src/main.c
@@ -3,40 +3,57 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define DELAY 50000
+#define DELAY 100000
 #define JUMP_STRENGTH -3
-#define PIPE_GAP 8
-#define PIPE_WIDTH 3
 #define GRAVITY 1
+#define PIPE_GAP 10
+#define PIPE_WIDTH 3
+#define PIPE_COUNT 3
+#define PIPE_SPACING 50
 
-int bird_y, bird_x;
-int pipe_x, pipe_length;
+struct Bird{
+  int y, x;
+  int velocity;
+};
 
-int velocity;
-int score;
+struct Pipe {
+  int x;
+  int hole_y;
+};
+
+struct Bird bird;
+struct Pipe pipes[PIPE_COUNT];
+
+int score = 0;
 int game_over = 0;
 
 void init_game() {
   initscr();
   noecho();
-  curs_set(0);
+  curs_set(FALSE);
   timeout(0);
 
-  bird_y = LINES / 2;
-  bird_x = COLS / 4;
-  pipe_x = COLS - 1;
-  pipe_length = rand() % (LINES - PIPE_GAP - 2) + 1;
+  bird.y = LINES / 2;
+  bird.x = COLS / 4;
+  bird.velocity = 0;
+
+  for (int i = 0; i < PIPE_COUNT; i++) {
+    pipes[i].x = COLS + i * PIPE_SPACING;
+    pipes[i].hole_y = rand() % (LINES - PIPE_GAP - 2) + 1;
+  }
 }
 
 void draw_bird() {
-  mvprintw(bird_y, bird_x, "O");
+  mvprintw(bird.y, bird.x, "O");
 }
 
-void draw_pipe() {
-  for (int y = 0; y < LINES; y++) {
-    if (y < pipe_length || y > pipe_length + PIPE_GAP) {
-      for (int i = 0; i < PIPE_WIDTH; i++) {
-        mvprintw(y, pipe_x, "|");
+void draw_pipes() {
+  for (int i = 0; i < PIPE_COUNT; i++) {
+    for (int y = 0; y < LINES; y++) {
+      if (y < pipes[i].hole_y || y > pipes[i].hole_y + PIPE_GAP) {
+        for (int w = 0; w < PIPE_WIDTH; w++) {
+          mvprintw(y, pipes[i].x + w, "###");
+        }
       }
     }
   }
@@ -44,25 +61,32 @@ void draw_pipe() {
 
 void update_game() {
   int ch = getch();
-  if (ch == ' ' && !game_over) velocity = JUMP_STRENGTH;
-  if (ch == 'q') game_over = 1;
+  if (ch == ' ' && !game_over) bird.velocity = JUMP_STRENGTH;
+  if (ch == 'q') game_over = 1;  // 'q'で終了
 
   if (!game_over) {
-    bird_y += velocity;
-    velocity += GRAVITY;
+    bird.y += bird.velocity;
+    bird.velocity += GRAVITY;
 
-    if (bird_y <= 0 || bird_y >= LINES - 1) game_over = 1;
+    // 鳥の地面・天井衝突判定
+    if (bird.y <= 0 || bird.y >= LINES - 1) game_over = 1;
+    
+    // 土管の移動
+    for (int i = 0; i < PIPE_COUNT; i++) {
+      pipes[i].x--;
 
-    pipe_x--;
-    if (pipe_x <= 0) {
-      pipe_x = COLS - 1;
-      pipe_length = rand() % (LINES - PIPE_GAP - 2) + 1;
-      score++;
-    }
+    // 画面外に出たらリセット
+      if (pipes[i].x <= 0) {
+        pipes[i].x = COLS - 1;
+        pipes[i].hole_y = rand() % (LINES - PIPE_GAP - 2) + 1;
+        score++;
+      }
 
-    if (bird_x >= pipe_x && bird_x < pipe_x + PIPE_WIDTH) {
-      if (bird_y < pipe_length || bird_y > pipe_length + PIPE_GAP) {
-        game_over = 1;
+      // 当たり判定
+      if (bird.x >= pipes[i].x && bird.x < pipes[i].x + PIPE_WIDTH) {
+        if (bird.y < pipes[i].hole_y || bird.y > pipes[i].hole_y + PIPE_GAP) {
+          game_over = 1;
+        }
       }
     }
   }
@@ -71,7 +95,7 @@ void update_game() {
 void draw_game() {
   clear();
   draw_bird();
-  draw_pipe();
+  draw_pipes();
   mvprintw(0, 0, "Score: %d", score);
   if (game_over) mvprintw(LINES / 2, COLS / 2 - 5, "GAME OVER! Press 'q' to exit.");
   refresh();
